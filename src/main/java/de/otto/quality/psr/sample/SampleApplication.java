475626e6b2670c77db62adfdd7851612fb1ad912
m31 <search@meyling.com>
2019-01-30 17:32:14 +0100

    just a little bit more

--- src/main/java/de/otto/quality/psr/sample/SampleApplication.java
+++ src/main/java/de/otto/quality/psr/sample/SampleApplication.java
@@ -1,12 +1,19 @@
 package de.otto.quality.psr.sample;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.logging.logback.LogbackLoggingSystem;
 
 @SpringBootApplication
 public class SampleApplication {
 
+	public final static Logger logger = LoggerFactory.getLogger(SampleApplication.class);
+
+
 	public static void main(String[] args) {
+		logger.info("I am starting {}", args);
 		SpringApplication.run(SampleApplication.class, args);
 	}
 
